b6641be14af3a097a93b69c22ddb81eae0f4a415
EvgenStr <juve2910@gmail.com>
2021-04-18 12:47:01 +0300

    testing isAnagrams

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,18 @@
 Регистр букв не имеет значения. */
 
 function isAnagrams(word1, word2) {
-  if (typeof word1 !== 'string' || typeof word2 !== 'string') {
-    throw new TypeError('Words must be a string type');
-  }
-  if (!word1.length|| !word2.length) throw new RangeError(`${word1.length?'second word empty':'first word empty'}`);
-  if (word1.length !== word2.length) return false;
-  
-
+  if (typeof word1 !== 'string' || typeof word2 !== 'string') throw new TypeError('Words must be a string type');
+  const trimmedWord1 = word1.trim();
+  const trimmedWord2 = word2.trim();
+  if (!trimmedWord1.length || !trimmedWord2.length) throw new RangeError(`${trimmedWord1.length ? 'second word empty' : 'first word empty'}`);
+  if (trimmedWord1.length !== trimmedWord2.length) return false;
+  let result = trimmedWord1.toLowerCase().split("");
+  trimmedWord2.toLowerCase().split("").forEach(elem => result.indexOf(elem) >= 0 ? result.splice(result.indexOf(elem), 1) : null);
+  return !result.length;
 }
+
+console.log(isAnagrams("   test ", " tetsfewf "));
+
+//2. Написать функцию, которая подсчитывает количество гласных в строке.
+
+function countVowels(){}
